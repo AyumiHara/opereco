eae75d351aae63ebe80b557bca01fe7eaf3ab9c3
AyumiHara <mapledog0425@gmail.com>
2016-11-20 15:45:10 +0900

    Initial Commit

---
+++ opereko/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  opereko
+//
+//  Created by 原 あゆみ on 2016/11/20.
+//  Copyright © 2016年 原 あゆみ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
